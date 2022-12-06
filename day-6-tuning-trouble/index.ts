4de6db6d3a6f64118bef2c1a808927e10c8e23fb
galnegus <tobiashindersson@hotmail.com>
2022-12-06 18:03:31 +0100

    day 6

---
+++ day-6-tuning-trouble/index.ts
@@ -0,0 +1,50 @@
+console.time("Execution time");
+
+const input: string = require("fs")
+  .readFileSync(require("path").resolve(__dirname, "input"), "utf-8");
+
+function findMarker(str: string, distinctChars: number): number {
+  const slidingCharCount = new Map<string, number>();
+  for (let end = 0; end < str.length; ++end) {
+    const start = end - distinctChars;
+
+    slidingCharCount.set(str[end], (slidingCharCount.get(str[end]) ?? 0) + 1);
+    if (end >= distinctChars) {
+      slidingCharCount.set(str[start], (slidingCharCount.get(str[start]) ?? 0) - 1);
+    }
+
+    if (
+      end >= distinctChars - 1 &&
+      hasDuplicatesInRange(str, end - distinctChars + 1, end, slidingCharCount)
+    ) { 
+      return end + 1;
+    }
+  }
+  throw new Error("Should've found something by now...");
+}
+function hasDuplicatesInRange(str: string, start: number, end: number, slidingCharCount: Map<string, number>): boolean {
+  for (let i = start; i <= end; ++i) {
+    if ((slidingCharCount.get(str[i]) ?? 0) > 1) return false;
+  }
+  return true;
+}
+
+function testFindMarker(): void {
+  console.assert(findMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4) === 7);
+  console.assert(findMarker("bvwbjplbgvbhsrlpgdmjqwftvncz", 4) === 5);
+  console.assert(findMarker("nppdvjthqldpwncqszvftbrmjlhg", 4) === 6);
+  console.assert(findMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4) === 10);
+  console.assert(findMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4) === 11);
+  console.assert(findMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14) === 19);
+  console.assert(findMarker("bvwbjplbgvbhsrlpgdmjqwftvncz", 14) === 23);
+  console.assert(findMarker("nppdvjthqldpwncqszvftbrmjlhg", 14) === 23);
+  console.assert(findMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14) === 29);
+  console.assert(findMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14) === 26);
+}
+
+testFindMarker();
+console.log("Part 1 answer:", findMarker(input, 4));
+console.log("Part 2 answer:", findMarker(input, 14));
+
+console.timeEnd("Execution time");
+export {};
