42afd0504a3e64289c4904a9ed6b8899f46099f5
galnegus <tobiashindersson@hotmail.com>
2022-12-09 14:55:55 +0100

    tidy day 9

--- day-9-rope-bridge/index.ts
+++ day-9-rope-bridge/index.ts
@@ -5,7 +5,14 @@ type Position = [number, number];
 type Direction = "U" | "R" | "D" | "L";
 type Move = [Direction, number];
 
-// For convenience/readability, so you can write position[x] and position[y].
+const directionDelta: Record<Direction, Position> = {
+  U: [0, 1],
+  R: [1, 0],
+  D: [0, -1],
+  L: [-1, 0],
+};
+
+// For convenience/readability, so that I can write position[x] and position[y].
 const x = 0;
 const y = 1;
 
@@ -16,8 +23,51 @@ const moves: Array<Move> = require("fs")
   .filter(Boolean)
   .map((line: string) => line.split(" ").map((value, index) => (index === 0 ? value : +value)));
 
+/**
+ * Offset is used to be able to handle negative numbers, since positions can have negative values (we start at [0, 0]).
+ * Largest step size seems to be around 20, and there's at most around 500 steps in any direction.
+ * So 16 bits per axis should be enough since (2^16)/2 > 20*500.
+ */
+const offset = (1 << 16) / 2;
+function posHash([x, y]: Position): number {
+  return x + offset + ((y + offset) << 16);
+}
+
+function numberOfVisitedPositions(numberOfKnots: number): number {
+  const knots: Array<Position> = new Array(numberOfKnots).fill(undefined).map(() => [0, 0]);
+  const visited = new Set<number>([posHash([0, 0])]);
+  for (const [direction, steps] of moves) {
+    for (let step = 0; step < steps; ++step) {
+      let [dx, dy] = directionDelta[direction];
+      knots[0][x] += dx;
+      knots[0][y] += dy;
+
+      for (let i = 1; i < knots.length; ++i) {
+        const prevKnot = knots[i - 1];
+        const currentKnot = knots[i];
+        const yMoved = Math.abs(prevKnot[y] - currentKnot[y]) > 1;
+        const xMoved = Math.abs(prevKnot[x] - currentKnot[x]) > 1;
+        if (!yMoved && !xMoved) continue;
+
+        dx = (!xMoved ? prevKnot[x] : prevKnot[x] - dx) - currentKnot[x];
+        dy = (!yMoved ? prevKnot[y] : prevKnot[y] - dy) - currentKnot[y];
+        currentKnot[x] += dx;
+        currentKnot[y] += dy;
+      }
+      visited.add(posHash(knots[knots.length - 1]));
+      printTestKnots(knots);
+    }
+  }
+  return visited.size;
+}
+
+console.log("Part 1 answer:", numberOfVisitedPositions(2));
+console.log("Part 2 answer:", numberOfVisitedPositions(10));
+
 /** Prints the "test" file positions to look like the examples in the assignment, just for debugging. */
 function printTestKnots(knots: Array<Position>): void {
+  if (inputFile !== "test") return;
+
   const board: Array<Array<string>> = new Array(5)
     .fill(undefined)
     .map(() => new Array(6).fill("."));
@@ -27,68 +77,6 @@ function printTestKnots(knots: Array<Position>): void {
   }
   console.log(board.map((row) => row.join("")).join("\n"), "\n");
 }
-
-/**
- * Offset is used to be able to handle negative numbers, since positions can have negative values (we start at [0, 0]).
- * Largest step size seems to be around 20, and there's at most around 500 steps in any direction.
- * So 16 bits per axis should be enough since (2^16)/2 > 20*500.
- */ 
-const offset = (1 << 16) / 2;
-function posHash([x, y]: Position): number {
-  return x + offset + ((y + offset) << 16);
-}
-
-function knotsTouch(head: Position, tail: Position): boolean {
-  return Math.abs(head[x] - tail[x]) <= 1 && Math.abs(head[y] - tail[y]) <= 1;
-}
-
-function visitedPositions(numberOfKnots: number): number {
-  const rope: Array<Position> = new Array(numberOfKnots).fill(undefined).map(() => [0, 0]);
-  const head = rope[0];
-  const tail = rope[rope.length - 1];
-  const visited = new Set<number>([posHash(tail)]);
-  for (const [direction, steps] of moves) {
-    for (let i = 0; i < steps; ++i) {
-      let prevX = head[x];
-      let prevY = head[y];
-
-      if (direction === "U") head[y] += 1;
-      if (direction === "R") head[x] += 1;
-      if (direction === "D") head[y] -= 1;
-      if (direction === "L") head[x] -= 1;
-
-      for (let knotIndex = 1; knotIndex < rope.length; ++knotIndex) {
-        const currentKnot = rope[knotIndex];
-        const prevKnot = rope[knotIndex - 1];
-        if (knotsTouch(prevKnot, currentKnot)) continue;
-
-        const nextPrevX = currentKnot[x];
-        const nextPrevY = currentKnot[y];
-        if (
-          Math.abs(prevKnot[y] - currentKnot[y]) > 1 &&
-          Math.abs(prevKnot[x] - currentKnot[x]) > 1
-        ) {
-          currentKnot[x] = prevX;
-          currentKnot[y] = prevY;
-        } else if (Math.abs(prevKnot[y] - currentKnot[y]) > 1) {
-          currentKnot[x] = prevKnot[x];
-          currentKnot[y] = prevY;
-        } else {
-          currentKnot[y] = prevKnot[y];
-          currentKnot[x] = prevX;
-        }
-        prevX = nextPrevX;
-        prevY = nextPrevY;
-      }
-      visited.add(posHash(tail));
-      if (inputFile === "test") printTestKnots(rope);
-    }
-  }
-  return visited.size;
-}
-
-console.log("Part 1 answer:", visitedPositions(2));
-console.log("Part 2 answer:", visitedPositions(10));
 
 console.timeEnd("Execution time");
 export {};
