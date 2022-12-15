d2fa4a96b88a65e9c163cc4078b4e0bcef27c447
galnegus <tobiashindersson@hotmail.com>
2022-12-16 00:00:03 +0100

    tidying a bit

--- day-15-beacon-exclusion-zone/index.ts
+++ day-15-beacon-exclusion-zone/index.ts
@@ -1,5 +1,9 @@
 console.time("Execution time");
 
+/**
+ * Takes around ~1s on my desktop, good enough I think :)
+ */
+
 interface Position {
   x: number;
   y: number;
@@ -38,26 +42,20 @@ const sensorReadings: Array<SensorReading> = input.split(/\r?\n/).map((line) => 
   };
 });
 
-function positionHash({ x, y }: Position): string {
-  return `${x},${y}`;
-}
-function part1(y: number): number {
+function findLinesAt(y: number, min: number, max: number): Array<Line> {
   const lines: Array<Line> = [];
-  const beacons = new Set<string>();
   for (const sensorReading of sensorReadings) {
-    const diff = sensorReading.distance - Math.abs(y - sensorReading.sensor.y);
-    if (diff < 0) continue;
+    const yDiff = sensorReading.distance - Math.abs(y - sensorReading.sensor.y);
+    if (yDiff < 0) continue;
     const sensorLine: Line = {
-      start: sensorReading.sensor.x - diff,
-      end: sensorReading.sensor.x + diff,
+      start: Math.max(sensorReading.sensor.x - yDiff, min),
+      end: Math.min(sensorReading.sensor.x + yDiff, max),
     };
-    const l = lines.length;
-    for (let i = 0; i < l; ++i) {
+    const numberOfLines = lines.length;
+    for (let i = 0; i < numberOfLines; ++i) {
       const otherLine = lines.shift();
       if (otherLine === undefined) throw new Error("typescript no");
-      const overlaps =
-        sensorLine.start - otherLine.end <= 1 && otherLine.start - sensorLine.end <= 1;
-      if (overlaps) {
+      if (sensorLine.start - otherLine.end <= 1 && otherLine.start - sensorLine.end <= 1) {
         sensorLine.start = Math.min(sensorLine.start, otherLine.start);
         sensorLine.end = Math.max(sensorLine.end, otherLine.end);
       } else {
@@ -65,51 +63,33 @@ function part1(y: number): number {
       }
     }
     lines.push(sensorLine);
+  }
+  return lines;
+}
+
+function numberOfBlockedPositions(y: number): number {
+  const lines: Array<Line> = findLinesAt(y, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY);
+  const beacons = new Set<string>();
+  for (const sensorReading of sensorReadings) {
     if (sensorReading.closestReading.y === y)
-      beacons.add(positionHash(sensorReading.closestReading));
+      beacons.add(`${sensorReading.closestReading.x},${sensorReading.closestReading.y}`);
   }
   return lines.reduce((sum, line) => sum + line.end - line.start + 1, 0) - beacons.size;
 }
-
-function part2(max: number): number | null {
+function findTuningFrequency(max: number): number {
   for (let y = 0; y <= max; ++y) {
-    const lines: Array<Line> = [];
-    for (const sensorReading of sensorReadings) {
-      const diff = sensorReading.distance - Math.abs(y - sensorReading.sensor.y);
-      if (diff < 0) continue;
-      const sensorLine: Line = {
-        start: sensorReading.sensor.x - diff,
-        end: sensorReading.sensor.x + diff,
-      };
-
-      sensorLine.start = Math.max(sensorLine.start, 0);
-      sensorLine.end = Math.min(sensorLine.end, max);
-
-      const l = lines.length;
-      for (let i = 0; i < l; ++i) {
-        const otherLine = lines.shift();
-        if (otherLine === undefined) throw new Error("typescript no");
-        const overlaps =
-          sensorLine.start - otherLine.end <= 1 && otherLine.start - sensorLine.end <= 1;
-        if (overlaps) {
-          sensorLine.start = Math.min(sensorLine.start, otherLine.start);
-          sensorLine.end = Math.max(sensorLine.end, otherLine.end);
-        } else {
-          lines.push(otherLine);
-        }
-      }
-      lines.push(sensorLine);
-    }
-    if (lines.length > 1) { // ASSUMPTION: The hidden beacon is in a gap between two "lines"
+    const lines: Array<Line> = findLinesAt(y, 0, max);
+    if (lines.length > 1) {
+      // ASSUMPTION: The hidden beacon is in a gap between two "lines"
       const x = (lines[0].start === 0 ? lines[0].end : lines[1].end) + 1;
       return x * 4000000 + y;
-    };
+    }
   }
-  return null;
+  return -1;
 }
 
-console.log("Part 1 answer:", part1(testInput ? 10 : 2000000));
-console.log("Part 2 answer:", part2(testInput ? 20 : 4000000));
+console.log("Part 1 answer:", numberOfBlockedPositions(testInput ? 10 : 2000000));
+console.log("Part 2 answer:", findTuningFrequency(testInput ? 20 : 4000000));
 
 console.timeEnd("Execution time");
 export {};
